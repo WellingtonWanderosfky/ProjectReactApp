d62378a5dfe9aaf32bb834c9aa3d2f93f264943a
Wellington Pedro <wellington.pedro@agenciam3.com>
2022-11-27 17:13:46 -0300

    completedChallengeCustom

---
+++ project-react-next/src/Components/CompletedChallenges/CompletedChallenges.tsx
@@ -0,0 +1,12 @@
+import styles from "./CompletedChallenges.module.scss"
+
+const CompletedChallenges = () => {
+  return (
+    <div className={styles.CompletedChallengesConteiner}>
+        <span >Desafios Completos</span>
+        <span>5</span>
+    </div>
+  )
+}
+
+export default CompletedChallenges
---
+++ project-react-next/src/Components/Profile/Profile.tsx
@@ -0,0 +1,17 @@
+import styles from "./Profile.module.scss";
+
+const Profile = () => {
+  return (
+    <div className={styles.profileContainer}>
+        <img src='https://github.com/WellingtonWanderosfky.png' alt='foto-perfil'/>
+        <div>
+            <strong>Wellington Wanderosky</strong>
+            <p>	
+              <img src="icons/up.svg" alt=""/>
+              Level 1 </p>
+        </div>
+    </div>
+  )
+}
+
+export default Profile;
