9389981e9b5f636c204e5b8a392486d4f137c4b2
Wellington Pedro <wellington.pedro@agenciam3.com>
2022-11-27 15:49:04 -0300

    fix componente topbar

---
+++ project-react-next/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import TopBar from "../Components/TopBar/TopBar";
+import styles from "../styles/main.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.conteiner}>
+      <TopBar/>
+    </div>
+  )
+}
