219d3f038f9a1a48c5df83831bc130182f0e90be
Wellington Pedro <wellington.pedro@agenciam3.com>
2022-11-27 17:10:07 -0300

    fix typescript

--- project-react-next/src/pages/index.tsx
+++ project-react-next/src/pages/index.tsx
@@ -1,10 +1,29 @@
+import Head from 'next/head';
+import CompletedChallenges from '../Components/CompletedChallenges/CompletedChallenges';
+import Profile from "../Components/Profile/Profile";
 import TopBar from "../Components/TopBar/TopBar";
 import styles from "../styles/main.module.scss";
 
+
 export default function Home() {
   return (
+    <><Head>
+
+    </Head>
     <div className={styles.conteiner}>
-      <TopBar/>
-    </div>
+        <TopBar />
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges/>
+          </div>
+
+          <div>
+
+          </div>
+
+        </section>
+      </div>
+    </>
   )
 }
