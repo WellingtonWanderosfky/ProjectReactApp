46fd3adfb0fc2b675e23cbc8412d7fcde6b804fb
Wellington Pedro <wellington.pedro@agenciam3.com>
2022-11-27 01:15:45 -0300

    install scss

---
+++ project-react/src/App.tsx
@@ -0,0 +1,14 @@
+import TopBar from './Components/TopBar/TopBar';
+import "./main.scss";
+
+function App() {
+  return (
+    <div className='conteiner'>
+      <div>
+        <TopBar/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ project-react/src/Components/TopBar/TopBar.tsx
@@ -0,0 +1,15 @@
+import "./TopBar.scss";
+
+const TopBar = () => {
+  return (
+    <header className="TopBarConteiner">
+        <span>0 xp</span>
+        <div className="ProgressBar">
+          <div className="ProgressSucess" style={{ width : "10%"}}></div>
+        </div>
+        <span>600 xp</span>
+    </header>
+  )
+}
+
+export default TopBar;
