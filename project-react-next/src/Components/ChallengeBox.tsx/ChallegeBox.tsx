408f6490f088e3ece0638514ee96e47149ba488e
Wellington Pedro <wellington.pedro@agenciam3.com>
2022-12-23 23:15:09 -0300

    fix: css desafio

--- project-react-next/src/Components/ChallengeBox.tsx/ChallegeBox.tsx
+++ project-react-next/src/Components/ChallengeBox.tsx/ChallegeBox.tsx
@@ -1,15 +1,42 @@
 import styles from "./Challegebox.module.scss";
 
 const ChallegenBox = () => {
+
+  const hasActiveChallenge = true;
+
+
   return (
     <div className={styles.ChallegeBoxConteiner}>
-      <div className={styles.ChallegeBoxNotActive}>
-        <strong>Finalize um ciclo para receber um desafio</strong>
-        <p>
-          <img src="icons/Up.svg" alt="Level Up" />
-          Avance de level completando desafios.
-        </p>
-      </div>
+      {hasActiveChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>
+            Ganhe 400 xp
+          </header>
+
+          <main>
+            <img src="icons/body.svg" alt="Body" />
+            <strong>Novo desafio</strong>
+            <p>Levante e faça uma caminhada de 3 minutos.</p>
+
+            <footer>
+              <button type="button" className={styles.challengeFailedButton}>
+                Falhei
+              </button>
+              <button type="button" className={styles.challengeSucceededButton}>
+                Completei
+              </button>
+            </footer>
+          </main>
+        </div>
+      )
+          : <div className={styles.ChallegeBoxNotActive}>
+          <strong>Finalize um ciclo para receber um desafio</strong>
+          <p>
+            <img src="icons/Up.svg" alt="Level Up" />
+            Avance de level completando desafios.
+          </p>
+        </div>
+      } 
     </div>
   )
 }
